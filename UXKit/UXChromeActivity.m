d14a2eb9ed9c3df90fc260efcb9052c6eec07c9f
Conor Mulligan <conmulligan@gmail.com>
2014-09-23 02:26:16 +0100

    Add Chrome activity

---
+++ UXKit/UXChromeActivity.m
@@ -0,0 +1,102 @@
+//
+//  UXChromeActivity.m
+//  UXKit
+//
+//  Copyright 2014 Conor Mulligan. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "UXChromeActivity.h"
+
+@interface UXChromeActivity ()
+
+@property (strong, nonatomic) NSURL *url;
+
+@end
+
+@implementation UXChromeActivity
+
+@synthesize url = _url;
+
+#pragma mark - UIActivity
+
+- (NSString *)activityType {
+    return NSStringFromClass([self class]);
+}
+
+- (NSString *)activityTitle {
+    return NSLocalizedString(@"Open in Chrome", @"");
+}
+
+- (UIImage *)activityImage {
+    NSString *path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"UXKit.bundle"];
+    NSBundle *bundle = [NSBundle bundleWithPath:path];
+    
+    return [UIImage imageWithContentsOfFile:[bundle pathForResource:@"Chrome" ofType:@"png"]];
+}
+
+- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
+    for (id activityItem in activityItems) {
+        NSURL *inputURL = [NSURL URLWithString:@"googlechrome://google.com"];
+        return [[UIApplication sharedApplication] canOpenURL:inputURL];
+        
+        if ([activityItem isKindOfClass:[NSURL class]] && [[UIApplication sharedApplication] canOpenURL:inputURL]) {
+            return YES;
+        }
+    }
+    
+    return NO;
+}
+
+- (void)prepareWithActivityItems:(NSArray *)activityItems {
+    for (id activityItem in activityItems) {
+        if ([activityItem isKindOfClass:[NSURL class]]) {
+            self.url = activityItem;
+        }
+    }
+}
+
+- (void)performActivity {
+    BOOL completed = NO;
+    
+    NSString *scheme = _url.scheme;
+    
+    NSString *chromeScheme = nil;
+    if ([scheme isEqualToString:@"http"]) {
+        chromeScheme = @"googlechrome";
+    } else if ([scheme isEqualToString:@"https"]) {
+        chromeScheme = @"googlechromes";
+    }
+    
+    if (chromeScheme) {
+        NSString *absoluteString = [_url absoluteString];
+        NSRange range = [absoluteString rangeOfString:@":"];
+        NSString *urlNoScheme = [absoluteString substringFromIndex:range.location];
+        NSString *chromeURLString = [chromeScheme stringByAppendingString:urlNoScheme];
+        NSURL *chromeURL = [NSURL URLWithString:chromeURLString];
+        
+        completed = [[UIApplication sharedApplication] openURL:chromeURL];
+    }
+    
+    [self activityDidFinish:completed];
+}
+
+@end
+
