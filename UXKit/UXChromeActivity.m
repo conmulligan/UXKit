fcd6b9bf6557b3b0ab1f180b0c2b2ceac7472e89
Conor Mulligan <conmulligan@gmail.com>
2015-05-13 14:29:25 +0100

    Fix issue with UXChromeActivity

--- UXKit/UXChromeActivity.m
+++ UXKit/UXChromeActivity.m
@@ -56,10 +56,6 @@
     for (id activityItem in activityItems) {
         NSURL *inputURL = [NSURL URLWithString:@"googlechrome://google.com"];
         return [[UIApplication sharedApplication] canOpenURL:inputURL];
-        
-        if ([activityItem isKindOfClass:[NSURL class]] && [[UIApplication sharedApplication] canOpenURL:inputURL]) {
-            return YES;
-        }
     }
     
     return NO;
